fe47e6d00698776b204d27d7d5a1f2d25cd77070
Takayuki YATO <tak_yato@yahoo.co.jp>
2021-02-28 20:41:34 +0900

    move shippori to develop

--- 00etc/shippori/00generate.sh
+++ 00etc/shippori/00generate.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -eu
 
+VERSION=3.000
 #perl gen-override.pl
 
-glymatch -v -r override-shippori.txt ShipporiMincho-Bold.ttf
-glymatch -v -r override-shippori.txt ShipporiMincho-ExtraBold.ttf
-glymatch -v -r override-shippori.txt ShipporiMincho-Medium.ttf
-glymatch -v -r override-shippori.txt ShipporiMincho-Regular.ttf
-glymatch -v -r override-shippori.txt ShipporiMincho-SemiBold.ttf
+glymatch -v -r override-shippori.txt --cmapver=$VERSION ShipporiMincho-Bold.ttf
+glymatch -v -r override-shippori.txt --cmapver=$VERSION ShipporiMincho-ExtraBold.ttf
+glymatch -v -r override-shippori.txt --cmapver=$VERSION ShipporiMincho-Medium.ttf
+glymatch -v -r override-shippori.txt --cmapver=$VERSION ShipporiMincho-Regular.ttf
+glymatch -v -r override-shippori.txt --cmapver=$VERSION ShipporiMincho-SemiBold.ttf
 
-mv Adobe-Japan1-* ../../shippori
-
-glymatch -v -j -r override-shippori.txt ShipporiMincho-Bold.ttf
-glymatch -v -j -r override-shippori.txt ShipporiMincho-Regular.ttf
+mv Adobe-Japan1-* ../../01develop/shippori-v$VERSION
